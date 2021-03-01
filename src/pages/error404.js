8847dab428548266cd6e3893c3a3de94c0cb1483
timmy_tunner_8 <timmytunnereight@gmail.com>
2021-02-28 18:43:09 -0600

    add page errror 404

---
+++ src/pages/error404.js
@@ -0,0 +1,11 @@
+const Error404 = () => {
+  const view = `
+        <div class="Error404">
+            <h2>Page no found</h2>
+        </div>
+    `;
+
+  return Error404;
+};
+
+export default Error404;
